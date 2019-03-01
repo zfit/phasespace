d5d3e6eeacd07843c804b784c3e1f75cf5b2ff31
Albert Puig <albert.puig@cern.ch>
2019-03-01 15:34:03 +0100

    Add 4-body test
    Fails!

--- scripts/prepare_test_samples.cxx
+++ scripts/prepare_test_samples.cxx
@@ -8,19 +8,18 @@
 Int_t N_EVENTS = 100000;
 
 
-int prepare_two_body()
+int prepare_two_body(TString filename)
 {
-    TFile out_file("../data/bto2pi.root", "RECREATE");
+    TFile out_file(filename, "RECREATE");
     TTree out_tree("events", "Generated events");
 
     TLorentzVector B(0.0, 0.0, 0.0, 5279.0);
-    Double_t masses[3] = {139.6, 139.6};
+    Double_t masses[2] = {139.6, 139.6};
 
-    if (!gROOT->GetClass("TGenPhaseSpace")) gSystem->Load("libPhysics");
     TGenPhaseSpace event;
     event.SetDecay(B, 2, masses);
 
-    TLorentzVector pion_1, pion_2, pion_3;
+    TLorentzVector pion_1, pion_2;
     Double_t weight;
     out_tree.Branch("pion_1", "TLorentzVector", &pion_1);
     out_tree.Branch("pion_2", "TLorentzVector", &pion_2);
@@ -40,15 +39,14 @@ int prepare_two_body()
 }
 
 
-int prepare_three_body()
+int prepare_three_body(TString filename)
 {
-    TFile out_file("../data/bto3pi.root", "RECREATE");
+    TFile out_file(filename, "RECREATE");
     TTree out_tree("events", "Generated events");
 
     TLorentzVector B(0.0, 0.0, 0.0, 5279.0);
     Double_t masses[3] = {139.6, 139.6, 139.6};
 
-    if (!gROOT->GetClass("TGenPhaseSpace")) gSystem->Load("libPhysics");
     TGenPhaseSpace event;
     event.SetDecay(B, 3, masses);
 
@@ -75,10 +73,49 @@ int prepare_three_body()
 }
 
 
-int prepare_test_samples(){
+int prepare_four_body(TString filename)
+{
+    TFile out_file(filename, "RECREATE");
+    TTree out_tree("events", "Generated events");
 
-    prepare_two_body();
-    prepare_three_body();
+    TLorentzVector B(0.0, 0.0, 0.0, 5279.0);
+    Double_t masses[4] = {139.6, 139.6, 139.6, 139.6};
+
+    TGenPhaseSpace event;
+    event.SetDecay(B, 4, masses);
+
+    TLorentzVector pion_1, pion_2, pion_3, pion_4;
+    Double_t weight;
+    out_tree.Branch("pion_1", "TLorentzVector", &pion_1);
+    out_tree.Branch("pion_2", "TLorentzVector", &pion_2);
+    out_tree.Branch("pion_3", "TLorentzVector", &pion_3);
+    out_tree.Branch("pion_4", "TLorentzVector", &pion_4);
+    out_tree.Branch("weight", &weight, "weight/D");
+
+
+    for (Int_t n=0; n<N_EVENTS; n++){
+        weight = event.Generate();
+        TLorentzVector *p_1 = event.GetDecay(0);
+        pion_1.SetPxPyPzE(p_1->Px(), p_1->Py(), p_1->Pz(), p_1->E());
+        TLorentzVector *p_2 = event.GetDecay(1);
+        pion_2.SetPxPyPzE(p_2->Px(), p_2->Py(), p_2->Pz(), p_2->E());
+        TLorentzVector *p_3 = event.GetDecay(2);
+        pion_3.SetPxPyPzE(p_3->Px(), p_3->Py(), p_3->Pz(), p_3->E());
+        TLorentzVector *p_4 = event.GetDecay(3);
+        pion_4.SetPxPyPzE(p_4->Px(), p_4->Py(), p_4->Pz(), p_4->E());
+        out_tree.Fill();
+    }
+    out_file.Write();
+    return 0;
+}
+
+
+int prepare_test_samples(TString two_body_file, TString three_body_file, TString four_body_file){
+    if (!gROOT->GetClass("TGenPhaseSpace")) gSystem->Load("libPhysics");
+
+    prepare_two_body(two_body_file);
+    prepare_three_body(three_body_file);
+    prepare_four_body(four_body_file);
 
     return 0;
 }
--- tests/test_physics.py
+++ tests/test_physics.py
@@ -13,6 +13,10 @@ import platform
 import numpy as np
 from scipy.stats import ks_2samp
 
+if platform.system() == 'Darwin':
+    import matplotlib
+    matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 import uproot
 
@@ -43,8 +47,12 @@ def create_ref_histos(n_pions):
     if not os.path.exists(ref_file):
         script = os.path.join(BASE_PATH,
                               'scripts',
-                              'prepare_test_samples.cxx')
-        os.system('root -qb {}+'.format(script))
+                              'prepare_test_samples.cxx+({})'.format(','.join(
+                                  '"{}"'.format(os.path.join(BASE_PATH,
+                                                             'data',
+                                                             'bto{}pi.root'.format(i+1))
+                                                for i in range(4)))))
+        os.system("root -qb '{}'".format(script))
     events = uproot.open(ref_file)['events']
     pion_names = ['pion_{}'.format(pion+1) for pion in range(n_pions)]
     pions = {pion_name: events.array(pion_name)
@@ -78,11 +86,6 @@ def test_two_body():
                          for coord, _ in enumerate(histos)] +
                         [ks_2samp(weight_histos, ref_weights)[1]])
     # Let's plot
-    if platform.system() == 'Darwin':
-        import matplotlib
-        matplotlib.use('TkAgg')
-    import matplotlib.pyplot as plt
-
     x = np.linspace(-3000, 3000, 100)
     if not os.path.exists(PLOT_DIR):
         os.mkdir(PLOT_DIR)
@@ -115,11 +118,6 @@ def test_three_body():
                          for coord, _ in enumerate(histos)] +
                         [ks_2samp(weight_histos, ref_weights)[1]])
     # Let's plot
-    if platform.system() == 'Darwin':
-        import matplotlib
-        matplotlib.use('TkAgg')
-    import matplotlib.pyplot as plt
-
     x = np.linspace(-3000, 3000, 100)
     if not os.path.exists(PLOT_DIR):
         os.mkdir(PLOT_DIR)
@@ -138,9 +136,44 @@ def test_three_body():
     assert np.all(p_values > 0.05)
 
 
+def test_four_body():
+    """Test B -> pi pi pi pi decay."""
+    weights, particles = tf.Session().run(tfphasespace.generate(B_AT_REST,
+                                                                [PION_MASS, PION_MASS, PION_MASS, PION_MASS],
+                                                                100000))
+    parts = np.concatenate(particles, axis=0)
+    histos = [make_norm_histo(parts[coord],
+                              range_=(-3000, 3000),
+                              weights=weights)
+              for coord in range(parts.shape[0])]
+    weight_histos = make_norm_histo(weights, range_=(0, 1))
+    ref_histos, ref_weights = create_ref_histos(4)
+    p_values = np.array([ks_2samp(histos[coord], ref_histos[coord])[1]
+                         for coord, _ in enumerate(histos)] +
+                        [ks_2samp(weight_histos, ref_weights)[1]])
+    # Let's plot
+    x = np.linspace(-3000, 3000, 100)
+    if not os.path.exists(PLOT_DIR):
+        os.mkdir(PLOT_DIR)
+    for coord, _ in enumerate(histos):
+        plt.hist(x, weights=histos[coord], alpha=0.5, label='tfphasespace', bins=100)
+        plt.hist(x, weights=ref_histos[coord], alpha=0.5, label='TGenPhasespace', bins=100)
+        plt.legend(loc='upper right')
+        plt.savefig(os.path.join(PLOT_DIR,
+                                 "four_body_pion_{}_{}.png".format(int(coord / 4) + 1,
+                                                                   ['x', 'y', 'z', 'e'][coord % 4])))
+        plt.clf()
+    plt.hist(np.linspace(0, 1, 100), weights=weight_histos, alpha=0.5, label='tfphasespace', bins=100)
+    plt.hist(np.linspace(0, 1, 100), weights=ref_weights, alpha=0.5, label='tfphasespace', bins=100)
+    plt.savefig(os.path.join(PLOT_DIR, 'four_body_weights.png'))
+    plt.clf()
+    assert np.all(p_values > 0.05)
+
+
 if __name__ == "__main__":
     test_two_body()
     test_three_body()
+    test_four_body()
 
 
 # EOF
