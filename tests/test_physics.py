99bd02b4475c44bcb883a7986068cf40229cfb4d
Albert Puig <albert.puig@cern.ch>
2019-03-01 16:40:18 +0100

    Refactor tests.

--- tests/test_physics.py
+++ tests/test_physics.py
@@ -26,12 +26,18 @@ from tfphasespace import tfphasespace
 
 
 BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-REF_FILE = os.path.join(BASE_PATH, 'data', 'bto3pi.root')
 PLOT_DIR = os.path.join(BASE_PATH, 'tests', 'plots')
 
 B_MASS = 5279.0
 B_AT_REST = tf.stack((0.0, 0.0, 0.0, B_MASS), axis=-1)
 PION_MASS = 139.6
+
+
+
+def make_norm_histo(array, range_, weights=None):
+    """Make histo and modify dimensions."""
+    histo = np.histogram(array, 100, range=range_, weights=weights)[0]
+    return histo/np.sum(histo)
 
 
 def create_ref_histos(n_pions):
@@ -58,116 +64,60 @@ def create_ref_histos(n_pions):
     pions = {pion_name: events.array(pion_name)
              for pion_name in pion_names}
     weights = events.array('weight')
-    weights_histo = np.histogram(weights, 100, range=(0, 1))[0]
-    return sum([make_histos(pion, range_=(-3000, 3000), weights=weights)
-                for pion in pions.values()], tuple()), \
-        weights_histo/np.sum(weights_histo)
+    return [make_norm_histo(array,
+                            range_=(-3000 if coord % 4 != 3 else 0, 3000),
+                            weights=weights)
+            for pion in pions.values()
+            for coord, array in enumerate([pion.x, pion.y, pion.z, pion.E])], \
+        make_norm_histo(weights, range_=(0, 1))
 
 
-def make_norm_histo(array, range_, weights=None):
-    """Make histo and modify dimensions."""
-    histo = np.histogram(array, 100, range=range_, weights=weights)[0]
-    return histo/np.sum(histo)
+def run_test(n_particles, test_prefix):
+    weights, particles = tf.Session().run(tfphasespace.generate(B_AT_REST,
+                                                                [PION_MASS] * n_particles,
+                                                                100000))
+    parts = np.concatenate(particles, axis=0)
+    histos = [make_norm_histo(parts[coord],
+                              range_=(-3000 if coord % 4 != 3 else 0, 3000),
+                              weights=weights)
+              for coord in range(parts.shape[0])]
+    weight_histos = make_norm_histo(weights, range_=(0, 1))
+    ref_histos, ref_weights = create_ref_histos(n_particles)
+    p_values = np.array([ks_2samp(histos[coord], ref_histos[coord])[1]
+                         for coord, _ in enumerate(histos)] +
+                        [ks_2samp(weight_histos, ref_weights)[1]])
+    # Let's plot
+    x = np.linspace(-3000, 3000, 100)
+    e = np.linspace(0, 3000, 100)
+    if not os.path.exists(PLOT_DIR):
+        os.mkdir(PLOT_DIR)
+    for coord, _ in enumerate(histos):
+        plt.hist(x if coord % 4 != 3 else e, weights=histos[coord], alpha=0.5, label='tfphasespace', bins=100)
+        plt.hist(x if coord % 4 != 3 else e, weights=ref_histos[coord], alpha=0.5, label='TGenPhasespace', bins=100)
+        plt.legend(loc='upper right')
+        plt.savefig(os.path.join(PLOT_DIR,
+                                 "{}_pion_{}_{}.png".format(test_prefix,
+                                                            int(coord / 4) + 1,
+                                                            ['x', 'y', 'z', 'e'][coord % 4])))
+        plt.clf()
+    plt.hist(np.linspace(0, 1, 100), weights=weight_histos, alpha=0.5, label='tfphasespace', bins=100)
+    plt.hist(np.linspace(0, 1, 100), weights=ref_weights, alpha=0.5, label='tfphasespace', bins=100)
+    plt.savefig(os.path.join(PLOT_DIR, '{}_weights.png'.format(test_prefix)))
+    plt.clf()
+    assert np.all(p_values > 0.05)
 
 
 def test_two_body():
     """Test B->pipi decay."""
-    weights, particles = tf.Session().run(tfphasespace.generate(B_AT_REST,
-                                                                [PION_MASS, PION_MASS],
-                                                                100000))
-    parts = np.concatenate(particles, axis=0)
-    histos = [make_norm_histo(parts[coord],
-                              range_=(-3000, 3000),
-                              weights=weights)
-              for coord in range(parts.shape[0])]
-    weight_histos = make_norm_histo(weights, range_=(0, 1))
-    ref_histos, ref_weights = create_ref_histos(2)
-    p_values = np.array([ks_2samp(histos[coord], ref_histos[coord])[1]
-                         for coord, _ in enumerate(histos)] +
-                        [ks_2samp(weight_histos, ref_weights)[1]])
-    # Let's plot
-    x = np.linspace(-3000, 3000, 100)
-    if not os.path.exists(PLOT_DIR):
-        os.mkdir(PLOT_DIR)
-    for coord, _ in enumerate(histos):
-        plt.hist(x, weights=histos[coord], alpha=0.5, label='tfphasespace', bins=100)
-        plt.hist(x, weights=ref_histos[coord], alpha=0.5, label='TGenPhasespace', bins=100)
-        plt.legend(loc='upper right')
-        plt.savefig(os.path.join(PLOT_DIR,
-                                 "two_body_pion_{}_{}.png".format(int(coord / 4) + 1,
-                                                                  ['x', 'y', 'z', 'e'][coord % 4])))
-        plt.clf()
-    plt.hist(np.linspace(0, 1, 100), weights=weight_histos, alpha=0.5, label='tfphasespace', bins=100)
-    plt.hist(np.linspace(0, 1, 100), weights=ref_weights, alpha=0.5, label='tfphasespace', bins=100)
-    assert np.all(p_values > 0.05)
-
+    run_test(2, "two_body")
 
 def test_three_body():
     """Test B -> pi pi pi decay."""
-    weights, particles = tf.Session().run(tfphasespace.generate(B_AT_REST,
-                                                                [PION_MASS, PION_MASS, PION_MASS],
-                                                                100000))
-    parts = np.concatenate(particles, axis=0)
-    histos = [make_norm_histo(parts[coord],
-                              range_=(-3000, 3000),
-                              weights=weights)
-              for coord in range(parts.shape[0])]
-    weight_histos = make_norm_histo(weights, range_=(0, 1))
-    ref_histos, ref_weights = create_ref_histos(3)
-    p_values = np.array([ks_2samp(histos[coord], ref_histos[coord])[1]
-                         for coord, _ in enumerate(histos)] +
-                        [ks_2samp(weight_histos, ref_weights)[1]])
-    # Let's plot
-    x = np.linspace(-3000, 3000, 100)
-    if not os.path.exists(PLOT_DIR):
-        os.mkdir(PLOT_DIR)
-    for coord, _ in enumerate(histos):
-        plt.hist(x, weights=histos[coord], alpha=0.5, label='tfphasespace', bins=100)
-        plt.hist(x, weights=ref_histos[coord], alpha=0.5, label='TGenPhasespace', bins=100)
-        plt.legend(loc='upper right')
-        plt.savefig(os.path.join(PLOT_DIR,
-                                 "three_body_pion_{}_{}.png".format(int(coord / 4) + 1,
-                                                                    ['x', 'y', 'z', 'e'][coord % 4])))
-        plt.clf()
-    plt.hist(np.linspace(0, 1, 100), weights=weight_histos, alpha=0.5, label='tfphasespace', bins=100)
-    plt.hist(np.linspace(0, 1, 100), weights=ref_weights, alpha=0.5, label='tfphasespace', bins=100)
-    plt.savefig(os.path.join(PLOT_DIR, 'three_body_weights.png'))
-    plt.clf()
-    assert np.all(p_values > 0.05)
-
+    run_test(3, "three_body")
 
 def test_four_body():
     """Test B -> pi pi pi pi decay."""
-    weights, particles = tf.Session().run(tfphasespace.generate(B_AT_REST,
-                                                                [PION_MASS, PION_MASS, PION_MASS, PION_MASS],
-                                                                100000))
-    parts = np.concatenate(particles, axis=0)
-    histos = [make_norm_histo(parts[coord],
-                              range_=(-3000, 3000),
-                              weights=weights)
-              for coord in range(parts.shape[0])]
-    weight_histos = make_norm_histo(weights, range_=(0, 1))
-    ref_histos, ref_weights = create_ref_histos(4)
-    p_values = np.array([ks_2samp(histos[coord], ref_histos[coord])[1]
-                         for coord, _ in enumerate(histos)] +
-                        [ks_2samp(weight_histos, ref_weights)[1]])
-    # Let's plot
-    x = np.linspace(-3000, 3000, 100)
-    if not os.path.exists(PLOT_DIR):
-        os.mkdir(PLOT_DIR)
-    for coord, _ in enumerate(histos):
-        plt.hist(x, weights=histos[coord], alpha=0.5, label='tfphasespace', bins=100)
-        plt.hist(x, weights=ref_histos[coord], alpha=0.5, label='TGenPhasespace', bins=100)
-        plt.legend(loc='upper right')
-        plt.savefig(os.path.join(PLOT_DIR,
-                                 "four_body_pion_{}_{}.png".format(int(coord / 4) + 1,
-                                                                   ['x', 'y', 'z', 'e'][coord % 4])))
-        plt.clf()
-    plt.hist(np.linspace(0, 1, 100), weights=weight_histos, alpha=0.5, label='tfphasespace', bins=100)
-    plt.hist(np.linspace(0, 1, 100), weights=ref_weights, alpha=0.5, label='tfphasespace', bins=100)
-    plt.savefig(os.path.join(PLOT_DIR, 'four_body_weights.png'))
-    plt.clf()
-    assert np.all(p_values > 0.05)
+    run_test(4, "four_body")
 
 
 if __name__ == "__main__":
