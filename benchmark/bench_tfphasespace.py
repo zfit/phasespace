fe80bbcda2d69154ba34d5bfd8ba39f2952003c8
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-03-04 14:11:54 +0100

    Update Benchmark: remove bug, create graph once, use Variable for n

--- benchmark/bench_tfphasespace.py
+++ benchmark/bench_tfphasespace.py
@@ -17,30 +17,48 @@ sys.path.append(os.path.dirname(__file__))
 
 from monitoring import Timer
 
+# to play around with optimization, no big effect though
+NUM_PARALLEL_EXEC_UNITS = 1
+config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=1,
+                        allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
+
 B_MASS = 5279.0
 B_AT_REST = tf.stack((0.0, 0.0, 0.0, B_MASS), axis=-1)
 PION_MASS = 139.6
 
-N_EVENTS = 1000000
-CHUNK_SIZE = 1000000
+N_EVENTS = 100000
+CHUNK_SIZE = 100000
 
-sess = tf.Session()
+N_EVENTS_VAR = tf.Variable(initial_value=N_EVENTS)
+CHUNK_SIZE_VAR = tf.Variable(initial_value=CHUNK_SIZE)
+
+samples = [tfphasespace.generate(B_AT_REST,
+                                 [PION_MASS, PION_MASS, PION_MASS],
+                                 CHUNK_SIZE_VAR)
+           for _ in range(0, N_EVENTS, CHUNK_SIZE)
+           ]
+
+sess = tf.Session(
+        config=config
+        )
+
+sess.run([N_EVENTS_VAR.initializer, CHUNK_SIZE_VAR.initializer])
 
 
 def test_three_body():
     """Test B -> pi pi pi decay."""
-
-    do_run()  # to get rid of initial overhead
+    with Timer(verbose=True):
+        print("Initial run (may takes more time than consequent runs)")
+        do_run()  # to get rid of initial overhead
     print("starting benchmark")
     with Timer(verbose=True):
-        do_run()
+        CHUNK_SIZE_VAR.load(CHUNK_SIZE + 1, session=sess)  # +1 to make sure we're not using any trivial caching
+        samples = do_run()
+        print("Shape of one particle momentum", samples[0][1][0].shape)
 
 
 def do_run():
-    sess.run([tfphasespace.generate(B_AT_REST,
-                                    [PION_MASS, PION_MASS, PION_MASS],
-                                    CHUNK_SIZE)
-              for _ in range(0, N_EVENTS + 1, CHUNK_SIZE)])
+    return sess.run(samples)
 
 
 if __name__ == "__main__":
