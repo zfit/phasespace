fdd88e064c62f47122527c3ea3a3d42d93c91542
Albert Puig <albert.puig@cern.ch>
2019-03-13 09:44:04 +0100

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
         test_suite='tests',
         tests_require=test_requirements,
         url='https://github.com/zfit/phasespace',
-        version='0.8.0',
+        version='0.9.0',
         zip_safe=False,
         )
