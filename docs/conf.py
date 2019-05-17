4187f43d93a0adbfd2043fd1c1276c4edee0d1db
Albert Puig <albert.puig@cern.ch>
2019-05-17 16:01:17 +0200

    Add missing import

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import phasespace
 
+import sphinx_bootstrap_theme
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
