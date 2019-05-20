81c1037d1b08c2e856a0422d2d6601541bc118e7
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-05-20 14:16:26 +0200

    release version 1.0.1

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zfit/phasespace',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
