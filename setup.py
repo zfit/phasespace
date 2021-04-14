42aa60fd9ecfaf546b9fc26e3a3dee35d9ddc388
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-04-14 12:55:44 +0200

    enh: update setuppy

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """The setup script."""
 
@@ -9,42 +8,39 @@ from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-with open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as history_file:
+with open(os.path.join(here, "CHANGELOG.rst"), encoding="utf-8") as history_file:
     history = history_file.read()
 
-with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as requirements_file:
+with open(
+        os.path.join(here, "requirements.txt"), encoding="utf-8"
+) as requirements_file:
     requirements = requirements_file.read().splitlines()
 
-with open(os.path.join(here, 'requirements_dev.txt'), encoding='utf-8') as requirements_dev_file:
+with open(
+        os.path.join(here, "requirements_dev.txt"), encoding="utf-8"
+) as requirements_dev_file:
     requirements_dev = requirements_dev_file.read().splitlines()
-
-# split the developer requirements into setup and test requirements
-if requirements_dev.count("") != 1 or requirements_dev.index("") == 0:
-    raise SyntaxError("requirements_dev.txt has the wrong format: setup and test "
-                      "requirements have to be separated by one blank line.")
-requirements_dev_split = requirements_dev.index("")
 
 
 setup(
-    long_description=readme.replace(":math:", "") + '\n\n' + history,
+    long_description=readme.replace(":math:", "") + "\n\n" + history,
     install_requires=requirements,
-    tests_require=['pytest',
-                   'pytest-xdist',
-                   'pytest-cov',
-                   'flaky',
-                   'coverage',
-                   'numpy',
-                   'matplotlib',
-                   'uproot',
-                   'uproot4',
-                   'scipy',
-                   'wget', ],
-
-    extras_require={
-        'dev': requirements_dev
-    },
+    tests_require=[
+        "pytest",
+        "pytest-xdist",
+        "pytest-cov",
+        "flaky",
+        "coverage",
+        "numpy",
+        "matplotlib",
+        "uproot",
+        "uproot4",
+        "scipy",
+        "wget",
+    ],
+    extras_require={"dev": requirements_dev},
     use_scm_version=True,
 )
