4456d8644e949713bb24cd3435875897914088bb
Remco de Boer <redeboer@gmx.com>
2021-05-19 18:21:09 +0200

    style: format rapidsim with black

--- tests/helpers/rapidsim.py
+++ tests/helpers/rapidsim.py
@@ -13,13 +13,13 @@ import numpy as np
 import uproot
 import uproot4
 
-BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-FONLL_FILE = os.path.join(BASE_PATH, 'data', 'fonll', 'LHC{}{}.root')
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+FONLL_FILE = os.path.join(BASE_PATH, "data", "fonll", "LHC{}{}.root")
 
 
 def get_fonll_histos(energy, quark):
     with uproot.open(FONLL_FILE.format(quark, int(energy))) as histo_file:
-        return histo_file['pT'], histo_file['eta']
+        return histo_file["pT"], histo_file["eta"]
 
 
 def generate_fonll(mass, beam_energy, quark, n_events):
@@ -43,18 +43,31 @@ def generate_fonll(mass, beam_energy, quark, n_events):
 
 def load_generated_histos(file_name, particles):
     with uproot4.open(file_name) as rapidsim_file:
-        return {particle: [rapidsim_file.get('{}_{}_TRUE'.format(particle, coord)).array(library='np')
-                           for coord in ('PX', 'PY', 'PZ', 'E')]
-                for particle in particles}
+        return {
+            particle: [
+                rapidsim_file.get("{}_{}_TRUE".format(particle, coord)).array(
+                    library="np"
+                )
+                for coord in ("PX", "PY", "PZ", "E")
+            ]
+            for particle in particles
+        }
 
 
 def get_tree(file_name, top_particle, particles):
     """Load a RapidSim tree."""
     with uproot4.open(file_name) as rapidsim_file:
-        tree = rapidsim_file['DecayTree']
-        return {particle: np.stack([1000.0 * tree['{}_{}_TRUE'.format(particle, coord)].array(library='np')
-                                    for coord in ('PX', 'PY', 'PZ', 'E')])
-                for particle in particles}
+        tree = rapidsim_file["DecayTree"]
+        return {
+            particle: np.stack(
+                [
+                    1000.0
+                    * tree["{}_{}_TRUE".format(particle, coord)].array(library="np")
+                    for coord in ("PX", "PY", "PZ", "E")
+                ]
+            )
+            for particle in particles
+        }
 
 
 def get_tree_in_b_rest_frame(file_name, top_particle, particles):
@@ -65,20 +78,27 @@ def get_tree_in_b_rest_frame(file_name, top_particle, particles):
             boostvector: boost vector. Can be either 3-vector or 4-vector (only spatial components
             are used)
         """
-        boost_vec = - boost[:3, :] / boost[3, :]
+        boost_vec = -boost[:3, :] / boost[3, :]
         b2 = np.sum(boost_vec * boost_vec, axis=0)
-        gamma = 1. / np.sqrt(1. - b2)
+        gamma = 1.0 / np.sqrt(1.0 - b2)
         gamma2 = (gamma - 1.0) / b2
         part_time = part_to_boost[3, :]
         part_space = part_to_boost[:3, :]
         bp = np.sum(part_space * boost_vec, axis=0)
-        return np.concatenate([part_space + (gamma2 * bp + gamma * part_time) * boost_vec,
-                               np.expand_dims(gamma * (part_time + bp), axis=0)],
-                              axis=0)
+        return np.concatenate(
+            [
+                part_space + (gamma2 * bp + gamma * part_time) * boost_vec,
+                np.expand_dims(gamma * (part_time + bp), axis=0),
+            ],
+            axis=0,
+        )
 
     part_dict = get_tree(file_name, top_particle, list(particles) + [top_particle])
     top_parts = part_dict.pop(top_particle)
-    return {part_name: lorentz_boost(part, top_parts)
-            for part_name, part in part_dict.items()}
+    return {
+        part_name: lorentz_boost(part, top_parts)
+        for part_name, part in part_dict.items()
+    }
+
 
 # EOF
