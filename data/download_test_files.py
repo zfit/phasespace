9ae95fdf4a0e59976556ff1c78d43a30e8001aec
Remco de Boer <redeboer@gmx.com>
2021-05-19 17:18:34 +0200

    style: format download script with black

--- data/download_test_files.py
+++ data/download_test_files.py
@@ -2,12 +2,23 @@ import wget
 from multiprocessing.pool import ThreadPool
 
 files_urls = [
-    ('B2K1Gamma_RapidSim_7TeV_K1KstarNonResonant_Tree.root',
-     'https://cernbox.cern.ch/index.php/s/8mN10X8U7VGfaRc/download'),
-    ('B2K1Gamma_RapidSim_7TeV_Tree.root', 'https://cernbox.cern.ch/index.php/s/pr3aM8n2hPT4Pag/download'),
-    ('B2KstGamma_RapidSim_7TeV_KstarNonResonant_Tree.root',
-     'https://cernbox.cern.ch/index.php/s/QuP2cHeISTTSLVv/download'),
-    ('B2KstGamma_RapidSim_7TeV_Tree.root', 'https://cernbox.cern.ch/index.php/s/EH5yrCpGko7P7Mc/download')]
+    (
+        "B2K1Gamma_RapidSim_7TeV_K1KstarNonResonant_Tree.root",
+        "https://cernbox.cern.ch/index.php/s/8mN10X8U7VGfaRc/download",
+    ),
+    (
+        "B2K1Gamma_RapidSim_7TeV_Tree.root",
+        "https://cernbox.cern.ch/index.php/s/pr3aM8n2hPT4Pag/download",
+    ),
+    (
+        "B2KstGamma_RapidSim_7TeV_KstarNonResonant_Tree.root",
+        "https://cernbox.cern.ch/index.php/s/QuP2cHeISTTSLVv/download",
+    ),
+    (
+        "B2KstGamma_RapidSim_7TeV_Tree.root",
+        "https://cernbox.cern.ch/index.php/s/EH5yrCpGko7P7Mc/download",
+    ),
+]
 
 
 def download(url_file_name):
@@ -18,7 +29,7 @@ def download(url_file_name):
     return file_name
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # files = ThreadPool(len(files_urls)).imap_unordered(download, files_urls)
     files = [download(url_file_name=url_file_name) for url_file_name in files_urls]
     for file in files:
