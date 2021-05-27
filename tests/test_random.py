a5d065418ad11346c2d091982102acffc3691be4
Remco de Boer <redeboer@gmx.com>
2021-05-27 13:33:51 +0200

    test: use uniform instead of uniform_full_int

--- tests/test_random.py
+++ tests/test_random.py
@@ -22,16 +22,16 @@ def create_from_seed_input():
 def test_get_rng(seed):
     rng1 = phsp.random.get_rng(seed())
     rng2 = phsp.random.get_rng(seed())
-    rnd1_seeded = rng1.uniform_full_int(shape=(100,))
-    rnd2_seeded = rng2.uniform_full_int(shape=(100,))
+    rnd1_seeded = rng1.uniform(shape=(100,))
+    rnd2_seeded = rng2.uniform(shape=(100,))
 
     rng3 = phsp.random.get_rng()
     rng4 = phsp.random.get_rng(seed())
     # advance rng4 by one step
     _ = rng4.split(1)
 
-    rnd3 = rng3.uniform_full_int(shape=(100,))
-    rnd4 = rng4.uniform_full_int(shape=(100,))
+    rnd3 = rng3.uniform(shape=(100,))
+    rnd4 = rng4.uniform(shape=(100,))
 
     np.testing.assert_array_equal(rnd1_seeded, rnd2_seeded)
     assert not np.array_equal(rnd1_seeded, rnd3)
