77830d13de3d09e56448086a083ef7b4028d2140
Simon Thor <simon11.thor@hotmail.se>
2021-10-03 16:55:14 +0200

    Remove newline

--- tests/fulldecay/example_decay_chains.py
+++ tests/fulldecay/example_decay_chains.py
@@ -11,7 +11,6 @@ dplus_decay = DecayMode(1, 'K- pi+ pi+ pi0', model='PHSP', zfit="relbw")
 pi0_decay = DecayMode(1, 'gamma gamma')
 dplus_single = DecayChain('D+', {'D+': dplus_decay, 'pi0': pi0_decay}).to_dict()
 
-
 # pi0 particle that can decay in 4 possible ways
 pi0_4branches = dfp.build_decay_chains('pi0')
 # Specify different mass functions for the different decays of pi0
