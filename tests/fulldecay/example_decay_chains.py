e610d11aaac3ae790efca0f236f1a990c0242d04
Simon Thor <simon11.thor@hotmail.se>
2021-10-03 16:54:41 +0200

    Add mass functions and make tests pass

--- tests/fulldecay/example_decay_chains.py
+++ tests/fulldecay/example_decay_chains.py
@@ -1,6 +1,9 @@
 from decaylanguage import DecayMode, DecayChain, DecFileParser
+import os.path
 
-dfp = DecFileParser("example_decays.dec")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+dfp = DecFileParser(f"{script_dir}/example_decays.dec")
 dfp.parse()
 
 # D+ particle with only one way of decaying
@@ -11,7 +14,10 @@ dplus_single = DecayChain('D+', {'D+': dplus_decay, 'pi0': pi0_decay}).to_dict()
 
 # pi0 particle that can decay in 4 possible ways
 pi0_4branches = dfp.build_decay_chains('pi0')
-# TODO add zfit mass functions
+# Specify different mass functions for the different decays of pi0
+mass_functions = ["relbw", "bw", "gauss"]
+for mass_function, decay_mode in zip(mass_functions, pi0_4branches["pi0"]):
+    decay_mode["zfit"] = mass_function
 
 # D+ particle that decays into 4 particles, out of which one particle in turn decays in 4 different ways.
 dplus_4grandbranches = dfp.build_decay_chains("D+")
